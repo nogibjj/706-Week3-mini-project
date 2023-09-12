92ca1787aace97b896b0ed97312a96d04ce39df2
hxia5 <hx96@duke.edu>
2023-09-12 17:56:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
-"""
-Main cli or app entry point
-"""
-
-from mylib.calculator import add
-import click
-
-#var=1;var=2
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+import pandas as pd
+import matplotlib.pyplot as plt
+from ydata_profiling import ProfileReport
+import polars as pl
 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+def polars_describe(csv):
+    polars_data = pl.read_csv(csv)
+    return polars_df.describe()
+
+
+def get_median(csv):
+    polars_data = pl.read_csv(csv)
+    return polars_df.median()
+
+
+def PlotShapeLeng(csv):
+    pd.set_option("display.max_columns", None)
+    polars_data = pl.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    plt.hist(general_df["Shape_Leng"], bins=20, edgecolor="black")
+    plt.title("Shape_Leng Distribution")
+    plt.xlabel("Shape_Leng")
+    plt.ylabel("Count")
+    plt.show()
+
+def PlotShapeArea(csv):
+    pd.set_option("display.max_columns", None)
+    polars_data = pl.read_csv(csv)
+    plt.figure(figsize=(10, 6))
+    plt.hist(general_df["Shape_Area"], bins=20, edgecolor="black")
+    plt.title("Shape_Area Distribution")
+    plt.xlabel("Shape_Area")
+    plt.ylabel("Count")
+    plt.show() 
+
+def polars_summary(csv):
+    polars_data = pl.scan_csv(csv)
+    profile = ProfileReport(polars_data, title="Profiling Report")
+    profile.to_file("polars.html")
