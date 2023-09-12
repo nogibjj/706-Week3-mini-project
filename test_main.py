904e3a89790d57d696f21dce6c1f0431bbf81967
hxia5 <hx96@duke.edu>
2023-09-12 18:07:56 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,12 +6,13 @@ from main import (
     polar_summary,
 )
 import pandas as pd
+import polars as pl
 
 example_csv = "https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv"
 
 def test_descirbe():
     data = pd.read_csv(example_csv)
-    result = Describe(data)
+    result = polar_describe(data)
     assert result.loc['count', 'Shape_Leng'] == 1189.0
     assert result.loc['mean', 'Shape_Area'] == 228153453.41946846
     assert result.loc['mean', 'Shape_Leng'] == 60622.47019343987
@@ -19,7 +20,7 @@ def test_descirbe():
     assert result.loc['std', 'Shape_Leng'] == 1094.0062261219582
 
 def test_median():
-    data = pd.read_csv(example_csv)
+    data = pl.read_csv(example_csv)
     assert data['Shape_Leng'].median() == 60896.5746
     assert data['Shape_Area'].median() == 230060526.252
 
@@ -31,4 +32,4 @@ def test_plot2():
 
 
 def test_generate_summary_report():
-    Summary(example_csv)
+    polar_summary(example_csv)
