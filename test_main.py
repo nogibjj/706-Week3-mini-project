f70f42e4b8efde0c4aedb6d9759d63dfcb17c4d1
hxia5 <hx96@duke.edu>
2023-09-12 20:46:32 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,10 +3,8 @@ from main import (
     get_median,
     PlotShapeLeng,
     PlotShapeArea,
-    polars_summary,
 )
 import polars as pl
-import pands as pd
 
 example_csv = "https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv"
 
