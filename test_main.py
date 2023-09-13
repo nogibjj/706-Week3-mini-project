c267e8fe707f3dffce594c6f18e02d5906e3e844
hxia5 <hx96@duke.edu>
2023-09-12 20:42:47 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,6 +6,7 @@ from main import (
     polars_summary,
 )
 import polars as pl
+import pands as pd
 
 example_csv = "https://catalogue.data.wa.gov.au/dataset/f39087e2-2885-473e-bc62-ca610cd94340/resource/96c892f3-b387-410c-80d0-e4dcec68e6f2/download/25ktopomapseriesindex.csv"
 
